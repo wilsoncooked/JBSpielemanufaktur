7c6b5a7d8a71d9727200b15cd1d9074e97976d91
Sarah Wilsoncook <sarahlouise125@gmail.com>
2021-10-12 09:05:29 +0200

    send form data with axios

--- src/lib/kontakt.svelte
+++ src/lib/kontakt.svelte
@@ -2,10 +2,12 @@
   import Section from '$lib/components/section.svelte'
   import { SerializedPrismicDOM } from '$lib/utils/helpers'
   import PrismicDom from 'prismic-dom'
+  import axios from 'axios'
 
   export let contentKontakt
-
-  const onSubmit = (event) => {
+  let formSubmittedSuccess = false
+  const handleSubmit = (event) => {
+    event.preventDefault()
     const formData = new FormData(event.target)
 
     const data = {}
@@ -13,7 +15,12 @@
       const [key, value] = field
       data[key] = value
     }
-    console.log(data)
+
+    axios.defaults.headers.post['Content-Type'] = 'application/json'
+    axios
+      .post('https://formsubmit.co/ajax/your@email.com', data)
+      .then((response) => (formSubmittedSuccess = true))
+      .catch((error) => console.log(error))
   }
 </script>
 
@@ -38,78 +45,79 @@
         />
       </div>
       <div class="w-full max-w-screen-sm">
-        <form
-          action="https://formsubmit.co/sarahlouise125@gmail.com"
-          method="POST"
-        >
-          <input type="hidden" name="_subject" value="New submission!" />
-          <input type="hidden" name="_captcha" value="false" />
-          <div class="flex w-full">
-            <div class="p-1.5 w-full">
-              <label for="vorname" class="hidden"
-                >{contentKontakt.data.vorname[0].text}</label
+        {#if formSubmittedSuccess}
+          <h3 class="font-mono text-2xl font-bold">Success!!!</h3>
+        {:else}
+          <form on:submit={handleSubmit} method="POST">
+            <input type="hidden" name="_subject" value="New submission!" />
+            <input type="hidden" name="_captcha" value="false" />
+            <div class="flex w-full">
+              <div class="p-1.5 w-full">
+                <label for="vorname" class="hidden"
+                  >{contentKontakt.data.vorname[0].text}</label
+                >
+                <input
+                  type="text"
+                  id="vorname"
+                  name="vorname"
+                  value=""
+                  placeholder={contentKontakt.data.vorname[0].text}
+                  required
+                  class="w-full bg-transparent px-4 pt-2 pb-1.5 placeholder-dark-desaturated-blue font-mono font-bold text-xl lg:text-2xl tracking-wider border-dark-desaturated-blue border rounded-xl hover:bg-white hover:bg-opacity-40 duration-100  focus:outline-none"
+                />
+              </div>
+              <div class="p-1.5 w-full">
+                <label for="nachname" class="hidden"
+                  >{contentKontakt.data.nachname[0].text}</label
+                >
+                <input
+                  type="text"
+                  id="nachname"
+                  name="nachname"
+                  value=""
+                  placeholder={contentKontakt.data.nachname[0].text}
+                  class="w-full bg-transparent px-4 pt-2 pb-1.5 placeholder-dark-desaturated-blue font-mono  font-bold text-xl lg:text-2xl tracking-wider border-dark-desaturated-blue border rounded-xl hover:bg-white hover:bg-opacity-40 duration-100  focus:outline-none"
+                />
+              </div>
+            </div>
+            <div class="p-1.5">
+              <label for="email" class="hidden"
+                >{contentKontakt.data.email[0].text}</label
               >
               <input
-                type="text"
-                id="vorname"
-                name="vorname"
+                type="email"
+                id="email"
+                name="email"
                 value=""
-                placeholder={contentKontakt.data.vorname[0].text}
+                placeholder={contentKontakt.data.email[0].text}
                 required
-                class="w-full bg-transparent px-4 pt-2 pb-1.5 placeholder-dark-desaturated-blue font-mono font-bold text-xl lg:text-2xl tracking-wider border-dark-desaturated-blue border rounded-xl hover:bg-white hover:bg-opacity-40 duration-100  focus:outline-none"
+                class="bg-transparent px-4 pt-2 pb-1.5 placeholder-dark-desaturated-blue font-mono font-bold text-xl lg:text-2xl tracking-wider border-dark-desaturated-blue border rounded-xl w-full hover:bg-white hover:bg-opacity-40 duration-100  focus:outline-none"
               />
             </div>
-            <div class="p-1.5 w-full">
-              <label for="nachname" class="hidden"
-                >{contentKontakt.data.nachname[0].text}</label
+            <div class="p-1.5">
+              <label for="comment" class="hidden"
+                >{contentKontakt.data.comment[0].text}</label
               >
-              <input
+              <textarea
                 type="text"
-                id="nachname"
-                name="nachname"
+                id="comment"
+                name="comment"
+                rows="4"
                 value=""
-                placeholder={contentKontakt.data.nachname[0].text}
-                class="w-full bg-transparent px-4 pt-2 pb-1.5 placeholder-dark-desaturated-blue font-mono  font-bold text-xl lg:text-2xl tracking-wider border-dark-desaturated-blue border rounded-xl hover:bg-white hover:bg-opacity-40 duration-100  focus:outline-none"
+                placeholder={contentKontakt.data.comment[0].text}
+                class="bg-transparent px-4 pt-2 pb-1.5 placeholder-dark-desaturated-blue font-mono font-bold text-xl lg:text-2xl tracking-wider border-dark-desaturated-blue border rounded-xl w-full hover:bg-white  hover:bg-opacity-40 duration-100  focus:outline-none"
               />
             </div>
-          </div>
-          <div class="p-1.5">
-            <label for="email" class="hidden"
-              >{contentKontakt.data.email[0].text}</label
-            >
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value=""
-              placeholder={contentKontakt.data.email[0].text}
-              required
-              class="bg-transparent px-4 pt-2 pb-1.5 placeholder-dark-desaturated-blue font-mono font-bold text-xl lg:text-2xl tracking-wider border-dark-desaturated-blue border rounded-xl w-full hover:bg-white hover:bg-opacity-40 duration-100  focus:outline-none"
-            />
-          </div>
-          <div class="p-1.5">
-            <label for="comment" class="hidden"
-              >{contentKontakt.data.comment[0].text}</label
-            >
-            <textarea
-              type="text"
-              id="comment"
-              name="comment"
-              rows="4"
-              value=""
-              placeholder={contentKontakt.data.comment[0].text}
-              class="bg-transparent px-4 pt-2 pb-1.5 placeholder-dark-desaturated-blue font-mono font-bold text-xl lg:text-2xl tracking-wider border-dark-desaturated-blue border rounded-xl w-full hover:bg-white  hover:bg-opacity-40 duration-100  focus:outline-none"
-            />
-          </div>
-          <div class="p-1.5 flex justify-end">
-            <button
-              type="submit"
-              class="uppercase px-8 py-2 text-dark-desaturated-blue font-mono border-dark-desaturated-blue font-bold text-xl lg:text-2xlß tracking-wider hover:bg-opacity-40 border rounded-xl hover:bg-white duration-100  focus:outline-none"
-            >
-              {contentKontakt.data.button_label[0].text}
-            </button>
-          </div>
-        </form>
+            <div class="p-1.5 flex justify-end">
+              <button
+                type="submit"
+                class="uppercase px-8 py-2 text-dark-desaturated-blue font-mono border-dark-desaturated-blue font-bold text-xl lg:text-2xlß tracking-wider hover:bg-opacity-40 border rounded-xl hover:bg-white duration-100  focus:outline-none"
+              >
+                {contentKontakt.data.button_label[0].text}
+              </button>
+            </div>
+          </form>
+        {/if}
       </div>
     </div>
   </div>
